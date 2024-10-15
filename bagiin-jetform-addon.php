f00d24c46ad54bfccb7c7c4c93119fb44dd69984
Henri Susanto <henry.dinus@gmail.com>
2024-10-16 06:04:49 +0700

    init

---
+++ bagiin-jetform-addon.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:         Bagiin Jetform Add-on
+ * Plugin URI:          https://github.com/susantohenri/bagiin-jetform-addon
+ * Description:         Jetform add on for bagiin to automate media deletion
+ * Version:             1.0.0
+ * Author:              henrisusanto
+ * Author URI:          https://github.com/susantohenri/
+ * Text Domain:         bagiin-jetform-addon
+ * License:             GPL-3.0+
+ * License URI:         http://www.gnu.org/licenses/gpl-3.0.txt
+ * Requires at least:    6.1
+ */
